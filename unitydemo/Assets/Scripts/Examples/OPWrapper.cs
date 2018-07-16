63f05f012d14e7e6e08d6a3e2aa73bd9e766c3dd
Tianyi Zhao <tz1@andrew.cmu.edu>
2018-07-16 16:30:07 -0400

    output control completed

---
+++ unitydemo/Assets/Scripts/Examples/OPWrapper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace opdemo.examples
+{
+    using opdemo.dll;
+
+    public class OPWrapper : OP_API
+    {
+        protected override void OPOutput(string message, int type = 0)
+        {
+            base.OPOutput(message, type);
+
+            OutputController.instance.PushNewOutput(message);
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            OPSetParameter(OPFlag.HAND);
+            OPRun();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ unitydemo/Assets/Scripts/Examples/OutputController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace opdemo.examples
+{
+    using opdemo.dll;
+
+    public class OutputController : MonoBehaviour
+    {
+        #region Singleton instance
+        public static OutputController instance { get; private set; }
+        private void Awake() {
+            if (instance = null) instance = this;
+            else
+            {
+                Destroy(instance);
+                instance = this;
+            }
+        }
+        #endregion
+
+        [SerializeField] List<HumanController2D> humans;
+
+        private OPFrame currentFrame;
+
+        public void PushNewOutput(string json)
+        {
+            currentFrame = OPFrame.FromJson(json);
+            for (int i = 0; i < currentFrame.units.Count; i++)
+            {
+                if (i > 2) break;
+                humans[0].PushNewOutputUnitData(currentFrame.units[0]);
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
